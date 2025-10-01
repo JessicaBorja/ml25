db8b0d79be2abef3a5681598f0d70c88cde7bfe2
Isaacc-0810 <73205107+Isaacc-0810@users.noreply.github.com>
2025-09-30 21:51:47 -0700

    P1 Exploration

--- src/ml25/P01_customer_purchases/data_exploration.py
+++ src/ml25/P01_customer_purchases/data_exploration.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import os
 from pathlib import Path
@@ -18,5 +20,19 @@ def read_csv(filename: str):
 if __name__ == "__main__":
     train_df = read_csv("customer_purchases_train")
     print(train_df.info())
+    print(train_df.describe(include="all"))
     test_df = read_csv("customer_purchases_test")
     print(test_df.columns)
+    numeric_cols = ["item_price", "item_avg_rating", "item_num_ratings", "customer_item_views", "purchase_item_rating"]
+    for col in numeric_cols:
+        if col in train_df.columns:
+            plt.figure(figsize=(6,4))
+            sns.histplot(train_df[col], bins=50)
+            plt.title(f"Distribution of {col}")
+            plt.show()
+    categorical_cols = train_df.select_dtypes(include=["object", "category"]).columns
+    for col in categorical_cols:
+        plt.figure(figsize=(6,4))
+        sns.countplot(y=train_df[col], order=train_df[col].value_counts().index)
+        plt.title(f"Counts of {col}")
+        plt.show()
